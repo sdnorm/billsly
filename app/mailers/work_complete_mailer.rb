8cbda994a2c0e3a70c1b77c5825692a315063c22
Spencer Norman <spencerdnorman@gmail.com>
2021-03-20 17:17:28 -0500

    justins email

--- app/mailers/work_complete_mailer.rb
+++ app/mailers/work_complete_mailer.rb
@@ -2,7 +2,7 @@ class WorkCompleteMailer < ApplicationMailer
   default from: "hello@billsly.co"
 
   def test
-    mail(to: "spencer@billsly.co",
+    mail(to: "justin@billsly.co",
     subject: "Tester Balls Emails")
   end
 
