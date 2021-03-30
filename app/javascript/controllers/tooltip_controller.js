b4dd45d918e2ebc33209e739dffa82ed8f5c7c79
Spencer Norman <spencerdnorman@gmail.com>
2021-03-30 14:05:16 -0500

    test client tooltip shows on page load

--- app/javascript/controllers/tooltip_controller.js
+++ app/javascript/controllers/tooltip_controller.js
@@ -4,6 +4,7 @@ import tippy from "tippy.js";
 export default class extends Controller {
   connect() {
     this.tippy = tippy(this.element);
+    this.tippy.show(this.element)
   }
 
   disconnect() {
