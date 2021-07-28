d1d6b5a59781bfff656ef0e937e3584a7d6a1418
Spencer Norman <spencerdnorman@gmail.com>
2021-07-28 13:03:40 -0500

    trying a config setting through the sidekiq yml setting

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,14 @@
 # # config/initializers/sidekiq.rb
  
-if Rails.env.production?
-  Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDIS_URL'],  
-      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
-  end
+# if Rails.env.production?
+#   Sidekiq.configure_client do |config|
+#     config.redis = { url: ENV['REDIS_URL'],  
+#       ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+#   end
 
-  Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDIS_URL'], 
-      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
-  end
-end
+#   Sidekiq.configure_server do |config|
+#     config.redis = { url: ENV['REDIS_URL'], 
+#       ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+#   end
+# end
 
