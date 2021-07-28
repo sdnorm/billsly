2178c0e90d7ce94f63f0e894f1256f823594655f
Spencer Norman <spencerdnorman@gmail.com>
2021-07-28 13:35:46 -0500

    trying Redis.current in the redis initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,4 @@
-$redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+Redis.current = Redis.new(
+  url: ENV["REDIS_URL"], 
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+)
